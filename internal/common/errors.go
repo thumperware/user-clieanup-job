89d05b04539202d6ffd728b91fd5140f432a1067
Hooman Familrouhani <hooman.fr@gmail.com>
2023-11-12 16:40:42 -0800

    removed unused file

--- internal/common/errors.go
+++
@@ -1,9 +0,0 @@
-package common
-
-// Error codes
-const (
-	ErrInvalidUserId   = "invalid_user_id"
-	ErrInvalidEmail    = "invalid_email"
-	ErrInvalidUserName = "invalid_user_name"
-	ErrInvalidPassword = "invalid_password"
-)
