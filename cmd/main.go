2f7cdfeadd2a19c53b85da2e2e78b9f1c688a449
Hooman Familrouhani <hooman.fr@gmail.com>
2023-11-12 16:21:18 -0800

    logging errors

--- cmd/main.go
+++ cmd/main.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"runtime"
 
-	"github.com/Neatolabs/golib/logging"
 	"github.com/thumperq/golib/config"
 	"github.com/thumperq/wms/user-clean-up-job/internal/application"
 	"github.com/thumperq/wms/user-clean-up-job/internal/infrastructure/db"
@@ -15,14 +14,9 @@ import (
 func main() {
 	ctx := context.Background()
 	configManager := config.NewConfigManager()
-	err := logging.SetupLogging(configManager)
-	if err != nil {
-		log.Printf("Error setting up logging: %v", err)
-		os.Exit(1)
-	}
 	dbFactory := db.NewDbFactory(configManager)
 	appFactory := application.NewApplicationFactory(dbFactory)
-	err = appFactory.UserApp.CleanupUsers(ctx)
+	err := appFactory.UserApp.CleanupUsers(ctx)
 	if err != nil {
 		log.Printf("Error invoking the cleanup users: %v", err)
 		os.Exit(1)
